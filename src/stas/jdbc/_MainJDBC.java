0fdc20a6f61ac6fb39ecbe12ef99fbed1cacd950
Stanislav Kardashov <stanislav.kardashov@gmail.com>
2017-07-09 19:50:29 +0300

    ssf

--- src/stas/jdbc/_MainJDBC.java
+++ src/stas/jdbc/_MainJDBC.java
@@ -19,7 +19,6 @@ public class _MainJDBC {
 			stmt.executeUpdate("CREATE TABLE animal (" + "id INTEGER PRIMARY KEY, " + "species_id integer, "
 					+ "name VARCHAR(255), " + "date_born TIMESTAMP)");
 			
-			
 			stmt.executeUpdate("INSERT INTO species VALUES (1, 'African Elephant', 7.5)");
 			stmt.executeUpdate("INSERT INTO species VALUES (2, 'Zebra', 1.2)");
 			stmt.executeUpdate("INSERT INTO animal VALUES (1, 1, 'Elsa', '2001−05−06 02:15')");
